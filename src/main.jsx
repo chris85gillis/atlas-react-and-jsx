ec1d5c52d7482f72c93e98652db3a53d797754c7
Chris Gillis <chris85gillis@gmail.com>
2024-08-27 16:24:53 -0500

    Task 0, React Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
 import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// Render the App component to the root element
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
